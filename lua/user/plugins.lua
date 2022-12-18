e7be7fc422d8fe8e24b4238467e9cd7b30eeb336
Robert Ulmer <robert@git.frontexperts.com>
2022-12-18 14:30:57 +0100

    flake8 formatting.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -110,6 +110,8 @@ return packer.startup(function(use)
 
   use {"vigoux/LanguageTool.nvim" }
 
+  -- Virtual Environment
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
